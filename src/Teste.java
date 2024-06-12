fa29932fb375f6c88e41c25226312a578f30f29f
marialuizamschmitt <maria-schmitt@educar.rs.gov.br>
2024-06-12 08:34:26 -0300

    testando commit

--- src/Teste.java
+++ src/Teste.java
@@ -3,6 +3,7 @@ public class Teste {
     public static void main(String[] args) {
         System.out.println("ola mundo");
         System.out.println("teste");
+        System.out.println("oi");
     }
 
 }
